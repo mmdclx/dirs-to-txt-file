51375d3dcbab994e1a067e9e8a2888b71a20aa5e
Mark C Mitchell <mark@markcmitchell.net>
2016-12-05 08:52:34 +1100

    move test dir const to outer scope of getAllDirsRecursively tests

--- test/index.js
+++ test/index.js
@@ -4,8 +4,11 @@ const getAllDirsRecursively = require('../getAllDirsRecursively')
 
 test('get-all-dirs-recursively', t => {
 
+  const testDir = __dirname + '/testDirectory'
+
+
   t.test('it will return, in an array, only directories', t => {
-    const testDir = __dirname + '/testDirectory'
+
     const dirs = getAllDirsRecursively(testDir)
 
     t.deepEqual(dirs, [
