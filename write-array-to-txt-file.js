e8037ed97e039f6a08bfb9ef3295360606f34749
Mark C Mitchell <mark@markcmitchell.net>
2016-12-04 18:33:17 +1100

    testing write-array-to-txt-file, works

--- write-array-to-txt-file.js
+++ write-array-to-txt-file.js
@@ -1,1 +1,17 @@
 #!/usr/bin/env node
+
+const fs = require('fs')
+const getAllDirsRecursively = require('./getAllDirsRecursively')
+
+let dirs = getAllDirsRecursively(__dirname)
+
+let ws = fs.createWriteStream(`${__dirname}/test.txt`)
+
+ws.on('end', () => {
+  console.log('ended writing stream')
+})
+
+dirs.forEach(dir => {
+  ws.write(dir + '\n')
+})
+ws.end()
