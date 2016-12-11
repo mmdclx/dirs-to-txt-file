6646590ac0e62a0beab1fad732df25e82d2387f8
Mark C Mitchell <mark@markcmitchell.net>
2016-12-11 12:34:56 +1100

    add package throught2 as dependency

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const getDirs = require('get-dirs')
 const writeArrayToTxtFile = require('./write-array-to-txt-file')
+const through2 = require('through2')
 
 module.exports = function dirsToTxtFile(rootDir, writeTo, exclude=[]) {
   let result = getDirs(rootDir, exclude)
