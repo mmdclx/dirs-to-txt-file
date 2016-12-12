57ba715d72eea0e50c615136ac2c39fb8e411043
Mark C Mitchell <mark@markcmitchell.net>
2016-12-12 14:41:44 +1100

    rewrite of dirs-to-txt-file using new get-dirs readableStream callback

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+const fs = require('fs')
 const getDirs = require('get-dirs')
-const writeArrayToTxtFile = require('./write-array-to-txt-file')
 const through2 = require('through2')
 
 module.exports = function dirsToTxtFile(rootDir, writeTo, exclude=[]) {
-  let result = getDirs(rootDir, exclude)
 
-  writeArrayToTxtFile(result, writeTo, () => {
-    console.log(`Successfully wrote to file ${writeTo}`)
+  let writeToFile = fs.createWriteStream(writeTo)
+
+  getDirs(rootDir, exclude, readableStream => {
+    readableStream
+      .pipe(through2(function transformChunkToNewLine(chunk, enc, done) {
+        this.push(chunk += '\n')
+        done()
+       }))
+      .pipe(writeToFile)
   })
+
 }
